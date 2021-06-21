94ff9f0c596b4d1677bac9d5897be27b14bad40b
Ancel <ancel.ajanga@student.moringaschool.com>
2021-06-21 16:06:18 +0300

    add js file elements

--- js/script.js
+++ js/script.js
@@ -1,21 +1,82 @@
-//    what we do section
-$('.main').click(function () {
-    $(this).children('.hide').slideToggle(1200);
-    $(this).children('.show').toggle(1500);
-  });
-  // giving feedback to the customer
-  $("button").click(function (omonge) {
-    var client = document.getElementById('nameDetail').value;
-    alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
-    omonge.preventDefault();
-  });
-  //    reset my form button
-  $("button").on('click', function () {
-    $('form').each(function () {
-      this.reset();
+$(function () {
+    let desp = $('#desp');
+    let devp = $('#devp');
+    let prop = $('#prop');
+    let destext = $('#destxt');
+    let devtext = $('#devtext');
+    let protext = $('#protext');
+
+    $(desp).hide();
+    $(devp).hide();
+    $(prop).hide();
+
+    //Show paragraph content
+    $('.des').on('click', function () {
+        $('.des').slideUp('slow');
+        $(desp).show(500);
+        $(destext).animate({fontWeight:'bolder',fontSize:'30px'})
+
     });
-  });
-  //portifolio section 
-  $(".card").hover(function () {
-    $(this).children(".card-1").fadeToggle(1000, "linear");
-  });
+    $('.dev').on('click', function () {
+        $('.dev').slideUp('slow');
+        $(devp).show(500);
+        $(devtext).animate({fontWeight:'bolder',fontSize:'30px'})
+    });
+    $('.pro').on('click', function () {
+        $('.pro').slideUp('slow');
+        $(prop).show(500);
+        $(protext).animate({fontWeight:'bolder',fontSize:'30px'})
+    });
+
+    //Hide paragraph content
+    $(desp, destext).on('click', function () {
+        $('.des').slideDown();
+        $(desp).hide(500);
+        $(destext).animate({fontWeight:'normal',fontSize:'16px'})
+    });
+    $(devp, devtext).on('click', function () {
+        $('.dev').slideDown();
+        $(devp).hide(500);
+        $(devtext).animate({fontWeight:'normal',fontSize:'16px'})
+    });
+    $(prop, protext).on('click', function () {
+        $('.pro').slideDown();
+        $(prop).hide(500);
+        $(protext).animate({fontWeight:'normal',fontSize:'16px'})
+    })
+
+    //Overlay images
+
+    $('.portimg').hover(function(){
+        $('.porttext',this).slideToggle('slow');
+     }, function(){
+        $('.porttext',this).slideToggle('slow');
+     });
+
+
+    //Form Validation
+        $("form#form").on('submit',function(event){
+            event.preventDefault();
+            let name = $("input#name").val();
+            let email = $("input#email").val();
+            let message = $("textarea#message").val();
+
+            if ($("input#name").val() && $("input#email").val()){
+                alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...stay safe.");
+            }
+            else {
+                alert("Please provide your correct name and email!");
+            }
+
+        });
+
+
+
+});
+
+function myFunction() {
+    let varName = document.getElementById("name").value;
+    //includes a line of code to auto capitalize the 1st letter of the users name.
+    const nameCaps = varName.charAt(0).toUpperCase() + varName.slice(1)
+    alert("Hey " + nameCaps + ". Thank you for reaching out to us.\nWe have received your message.\SHave a nice day");
+}
